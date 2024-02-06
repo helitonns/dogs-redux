b1ad83bd5237a212e2680460600303157e639b26
heliton <helitonns@gmail.com>
2024-02-06 11:56:06 -0400

    Primeiro commit - refatoração do projeto dogs

---
+++ src/Components/Photo/PhotoDelete.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./PhotoDelete.module.css";
+import { PHOTO_DELETE } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import { useNavigate } from 'react-router-dom';
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  async function handleClick() {
+    const confirm = window.confirm("Tem certeza que deseja deletar?");
+    if (confirm) {
+        const { url, options } = PHOTO_DELETE(id);
+        const {response} = await request(url, options);
+        if (response.ok) window.location.reload();
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;

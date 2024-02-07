6b23d40d16aaae5b0004c3bb0e69466d65037d85
heliton <helitonns@gmail.com>
2024-02-07 09:35:22 -0400

    Finalizado - não está funcionando como deveria

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,8 +2,10 @@ import { combineReducers, configureStore} from "@reduxjs/toolkit" ;
 import photo from './photo';
 import token from './token';
 import user from './user';
+import feed from './feed';
+import ui from './ui';
 
-const reducer = combineReducers({photo, token, user});
+const reducer = combineReducers({photo, token, user, feed, ui});
 //const store = configureStore({reducer, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat()});
 const store = configureStore({reducer});
 
---
+++ src/store/feed.js
@@ -0,0 +1,43 @@
+import createAsyncSlice from './helper/createAsync';
+import {PHOTOS_GET} from "../api";
+
+const slice = createAsyncSlice({
+  name: "feed",
+  initialState: {
+    list: [],
+    pages: 1,
+    infinite: true
+  },
+  reducers: {
+    addPhotos(state, action){
+      state.list.push(...action.payload);
+      console.log("Tamanho da lista: " + action.payload.length);
+      console.log(action.payload);
+      if(action.payload.length < 1) state.infite = false;
+    },
+    addPage(state){
+      state.pages++;
+    },
+    resetState(state){
+      state.infinite = true;
+      state.pages = 1;
+      state.list = [];
+      state.data = null;
+      state.error = null;
+      state.loading = false;
+    }
+  },
+  fetchConfig: ({page, total, user})=> PHOTOS_GET({page, total, user}),
+});
+
+export const fetchFeed = slice.asyncAction;
+export const {addPhotos, addPage, resetState: resetFeedState} = slice.actions;
+
+export const loadNewPhotos = ({total = 6, user})=> async (dispatch, getState)=> {
+  const {feed} = getState();
+  dispatch(addPage());
+  const {payload} = await dispatch(fetchFeed({page: feed.page, total, user}));
+  dispatch(addPhotos(payload));
+}
+
+export default slice.reducer;
